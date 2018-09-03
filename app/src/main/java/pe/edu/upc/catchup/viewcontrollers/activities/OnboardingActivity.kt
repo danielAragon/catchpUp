90e4e217fa60c12cac44285183eec86fd1aa1fc5
Angelo <u201316544@upc.edu.pe>
2018-09-03 08:55:26 -0500

    boilerplate

---
+++ app/src/main/java/pe/edu/upc/catchup/viewcontrollers/activities/OnboardingActivity.kt
@@ -0,0 +1,34 @@
+package pe.edu.upc.catchup.viewcontrollers.activities
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+
+import kotlinx.android.synthetic.main.activity_onboarding.*
+import pe.edu.upc.catchup.R
+
+class OnboardingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding)
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_onboarding, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
